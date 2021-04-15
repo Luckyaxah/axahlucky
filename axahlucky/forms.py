093ce440b4c144f0590281d69327150759f278f1
leqi <math_leqi@163.com>
2021-04-16 00:40:46 +0800

    .

--- axahlucky/forms.py
+++ axahlucky/forms.py
@@ -6,6 +6,9 @@ from wtforms import StringField, TextAreaField, SelectMultipleField, SubmitField
 class EditOpinionForm(FlaskForm):
     title = StringField('Title', validators=[])
     keyword = SelectMultipleField('Keyword', validators=[], coerce=int)
+    # from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField
+    # from axahlucky.models import Keyword
+    # keyword1 = QuerySelectMultipleField(query_factory=lambda: Keyword.query,get_label='content', allow_blank=True)
     content = CKEditorField('content', validators=[])
     submit = SubmitField()
 
